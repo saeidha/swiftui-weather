b66d0f87f7feb42cf6f832cc47fbef0128149692
saeed madi <150622499+madman9494@users.noreply.github.com>
2023-11-14 02:41:56 +0330

    Update Weather.swift

--- sample-weather/Model/Weather.swift
+++ sample-weather/Model/Weather.swift
@@ -19,3 +19,17 @@ struct Weather: Decodable{
 struct WeatherModel: Decodable{
     var main: String?
 }
+
+
+struct WeatherResponse: Decodable{
+    var main: Weather?
+    var weather: [WeatherModel]?
+}
+
+struct Weather: Decodable{
+    var temp: Double?
+}
+
+struct WeatherModel: Decodable{
+    var main: String?
+}
